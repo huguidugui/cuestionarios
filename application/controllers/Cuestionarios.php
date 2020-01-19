fa6cc07fa6f1108af42fe0f2452d34f7d41fc342
huguidugui <ringhugos@gmail.com>
2020-01-19 12:49:30 -0600

    entrar a cuestionarios WIP

--- application/controllers/Cuestionarios.php
+++ application/controllers/Cuestionarios.php
@@ -3,23 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cuestionarios extends CI_Controller {
 
-	public function __construct(){
+	private $data_session;
+
+	public function __construct()
+	{
 		parent::__construct();
-		$this->load->library('session');
+		$this->data_session = $this->session->userdata('ci_session');
+        if ($this->data_session === NULL) {
+        	redirect('/login/', 'location');
+        }
 	}
+	
 
 	public function index()
 	{
-		$data_session = $this->session->userdata('ci_session');
-        $usuarioId = $data_session['id'];
+		$data['titulo'] = 'Cuestionarios';
+		$data['sesion'] = $this->data_session;
+  
+		$this->load->view('headfoot/header', $data);
+		$this->load->view('templates/mostrar_cuestionarios', $data);
+		$this->load->view('headfoot/footer');
 
-        if ($data_session === NULL) {
-        	echo "No estás logueado";
-        	echo "<a href=". base_url('login') . ">Ir a login</a>";
-        } else {
-        	echo "Estás dentro del sistema";
-        	echo "<a href=". base_url('login/logout') . ">Salir</a>";
-        }
+
+
+        // if ($data_session === NULL) {
+        // 	echo "<br><br><br><br><br><br>No estás logueado";
+        // 	echo "<a href=". base_url('login') . ">Ir a login</a>";
+        // } else {
+        // 	echo "Estás dentro del sistema";
+        // 	echo "<a href=". base_url('login/logout') . ">Salir</a>";
+        // }
 	
 	}
 
--- application/views/headfoot/footer.php
+++ application/views/headfoot/footer.php
@@ -1,44 +1,65 @@
+    <!-- Start footer -->
+    <footer id="footer">
+        <div class="container">
+           <div class="row">
+               <div class="col-md-4 col-sm-4">
+                   <div class="footer-left">
+                     <p>Hugui Dugui </p>
+                     <p>México, CDMX &copy <?php echo date("Y"); ?></p>
+                   </div>
+               </div>
+               <div class="col-md-4 col-sm-4">
+                   <div class="footer-left text-center">
+                      <p>
+                        <a class="link-wa" href="https://api.whatsapp.com/send?phone=525572882186">
+                          <i class="fa fa-whatsapp"></i>
+                          044 55 7288 2186 
+                        </a>
+                      </p>
+
+                      <p>
+                        <i class="fa fa-envelope"></i>
+                        ringhugos@gmail.com
+                      </p>
+                   </div>
+               </div>
+               <div class="col-md-4 col-sm-4">
+                   <div class="footer-right">
+                     <a href="https://www.facebook.com/Hugo.Dugui" target="_blank">
+                        <i class="fa fa-facebook"></i>
+                      </a>
+                     <a href="https://twitter.com/huguidugui" target="_blank">
+                        <i class="fa fa-twitter"></i>
+                      </a>
+                      <a href="https://www.linkedin.com/in/hugo-martínez-gutiérrez-211732109" target="_blank">
+                        <i class="fa fa-linkedin"></i>
+                      </a>
+                   </div>
+               </div>
+           </div>
+        </div>
+    </footer>
+
+    <div class="container">
+        <div class="row">
+            <div id="scrollito" class="col-md-12 text-center">
+                
+            </div>
+        </div>
+    </div>
+    <!-- End footer -->
+
+    <script src="<?php echo base_url();?>assets/js/jquery-1.10.2.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
+    <script src="<?php echo base_url();?>assets/js/slick.js"></script>
+    <script src="<?php echo base_url();?>assets/js/waypoints.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.counterup.js"></script>
+    <script src="<?php echo base_url();?>assets/js/wow.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap-progressbar.js"></script>
+    <script src="<?php echo base_url();?>assets/js/jquery.animateNumber.js"></script>
 
 
-    <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery3-4-1.min.js"></script><!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap4/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/custom.js?ver=1.0"></script>
 
-    <script src="<?php echo base_url();?>assets/js/jquery.slimscroll.min.js"></script>
-	<script src="<?php echo base_url();?>assets/js/fastclick.js"></script>
-    <script src="<?php echo base_url();?>assets/js/adminlte.min.js"></script>
-	<script src="<?php echo base_url();?>assets/js/swal.min.js"></script>
-	<!-- <script src="<?php echo base_url();?>assets/js/dashboard.js"></script> -->
-	<script src="<?php echo base_url();?>assets/js/demo.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url();?>assets/js/wow.js"></script>
-    <?php
-        $method = $this->router->fetch_method();
-        $controller = $this->router->fetch_class();
-        if($controller == 'parrillas' && $method == 'crearParrillas') {  ?>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/vue.js"></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/vue-resource.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/app.js?ver=1.0"></script>
-    <?php } ?>
-
-
-    <?php
-        $method = $this->router->fetch_method();
-        $controller = $this->router->fetch_class();
-        if($controller == 'parrillas' && $method == 'crearParrillasComponentes') {  ?>
-            <script type="text/javascript" src="<?php echo base_url();?>assets/js/vue.js"></script>
-            <script type="text/javascript" src="<?php echo base_url();?>assets/js/vue-resource.min.js"></script>
-            <script type="text/javascript" src="<?php echo base_url();?>assets/js/componentes/app-crear-parrillas.js"></script>
-            <script type="text/javascript" src="<?php echo base_url();?>assets/js/componentes/app-crear-items.js"></script>
-            <script type="text/javascript" src="<?php echo base_url();?>assets/js/componentes/app-marvel.js"></script>
-            <script type="text/javascript" src="<?php echo base_url();?>assets/js/componentes/app.js"></script>
-    <?php } ?>
-
-    <?php
-      if(strpos(current_url(), 'implementacion')) { ?>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/vue.js"></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/vue-resource.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url();?>assets/js/app-implementacion.js"></script>
-    <?php } ?>
-    </body>
+  </body>
 </html>
