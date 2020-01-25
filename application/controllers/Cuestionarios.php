e056f504186f1b74fce370e0f022f8d9200c82a4
huguidugui <ringhugos@gmail.com>
2020-01-25 13:29:26 -0600

    Verbos model y mostrar temas en mostrar princpial

--- application/controllers/Cuestionarios.php
+++ application/controllers/Cuestionarios.php
@@ -1,17 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Cuestionarios extends CI_Controller {
+class Cuestionarios extends CI_Controller 
+{
 
 	private $data_session;
 
 	public function __construct()
 	{
+		// Library 'session' se carga globalmente desde autoload.php 
 		parent::__construct();
 		$this->data_session = $this->session->userdata('ci_session');
         if ($this->data_session === NULL) {
         	redirect('/login/', 'location');
         }
+        $this->load->model('Verbos_model');
 	}
 	
 	/*
@@ -20,23 +23,21 @@ class Cuestionarios extends CI_Controller {
 	public function index()
 	{
 		$data['titulo'] = 'Cuestionarios';
-		$data['sesion'] = $this->data_session;
+		// Verbos_model -> obtener recibe:
+		// $tabla = '',
+		// $campos = array(),
+		// $where = array(), 
+		// $columnOrder = '', 
+		// $order = 'ASC', 
+		// $start = 0, 
+		// $end = 0
+		$data['cuestionarios'] = $this->Verbos_model->obtener( 'temas', array(), array(), '', '', 0, 0 );
   
 		$this->load->view('headfoot/header', $data);
 		$this->load->view('headfoot/menu');
 		$this->load->view('templates/mostrar_cuestionarios');
 		$this->load->view('headfoot/footer');
-
-
-
-        // if ($data_session === NULL) {
-        // 	echo "<br><br><br><br><br><br>No estás logueado";
-        // 	echo "<a href=". base_url('login') . ">Ir a login</a>";
-        // } else {
-        // 	echo "Estás dentro del sistema";
-        // 	echo "<a href=". base_url('login/logout') . ">Salir</a>";
-        // }
 	
-	}
+	} //index
 
 } //Fin Cuestionarios
